a76a7ca794ac8caa95685e404086cc437c9a45e9
Shadov64 <oskar.janik@teb.edu.pl>
2025-05-17 15:19:36 +0200

    fixxx

--- next.config.mjs
+++ next.config.mjs
@@ -6,4 +6,20 @@ const nextConfig = {
   ignoreBuildErrors: true,
 }
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload({
+  ...nextConfig,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  devBundleServerPackages: false,
+})
