5144f48eb885fdabf4e2697effae30d7513fbcc4
Shadov64 <oskar.janik@teb.edu.pl>
2025-05-17 12:45:26 +0200

    next fix

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -1,7 +1,7 @@
-import type { LightbulbIcon as LucideProps } from "lucide-react"
+import type { LightbulbIcon as LucideProps } from 'lucide-react'
 
 export const Icons = {
-  logo: (props: LucideProps) => (
+  logo: (props: typeof LucideProps) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
@@ -16,9 +16,6 @@ export const Icons = {
       <path d="M18 9h1.5a2.5 2.5 0 0 0 0-5H18" />
       <path d="M4 22h16" />
       <path d="M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22" />
-      <path d="M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22" />
-      <path d="M18 2H6v7a6 6 0 0 0 12 0V2Z" />
     </svg>
   ),
 }
-
